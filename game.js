67f66da7555d54481adf467157ecdf6f25ec20a8
Jonathan Lopes <jonathanfiles10@gmail.com>
2021-12-19 23:02:19 -0300

    Verificando vencedor

--- game.js
+++ game.js
@@ -39,7 +39,27 @@ function inicializarEspacos(){
                     playTime = player1;
                 }
                 atualizaMostrador();
+                verificarVencedor();
             }
         });
     }
+}
+
+function verificarvencer(){
+    var a1 = document.getElementById("a1").getAttribute("jogada");
+    var a2 = document.getElementById("a2").getAttribute("jogada");
+    var a3 = document.getElementById("a3").getAttribute("jogada");
+
+    var b1 = document.getElementById("b1").getAttribute("jogada");
+    var b2 = document.getElementById("b2").getAttribute("jogada");
+    var b3 = document.getElementById("b3").getAttribute("jogada");
+
+    var c1 = document.getElementById("c1").getAttribute("jogada");
+    var c2 = document.getElementById("c2").getAttribute("jogada");
+    var c3 = document.getElementById("c3").getAttribute("jogada");
+
+    var vencedor = "";
+
+    if ((a1 == b1 && a1 == c1 && a1 != "" )  ||  (a1 == a2 && a1 == a3 && a1 != "" )  ||  (a1 == b2 && a1 == c3 && a1 != "" ))
+        vencedor = a1;
 }
